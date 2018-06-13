a6d56d42cc63221decc20430402a813cc5ee6bac
LeTanThanh <tanthanhcr95@gmail.com>
2018-06-13 20:14:14 +0700

    Add model book

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,8 @@
 10.times do
   User.create name: FFaker::Name.name, admin: FFaker::Boolean.random
 end
+
+10.times do
+  user_id = rand(User.count) + 1
+  Book.create user_id: user_id, author: FFaker::Book.author, title: FFaker::Book.title, description: FFaker::Book.description
+end
